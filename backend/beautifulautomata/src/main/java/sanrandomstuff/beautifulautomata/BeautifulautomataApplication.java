e16b46991ac60c2279766618964d0ca163a00018
Kutlu San Demiroren <Demiroren@student.tudelft.nl>
2025-07-20 12:22:03 +0200

    initial commit

---
+++ backend/beautifulautomata/src/main/java/sanrandomstuff/beautifulautomata/BeautifulautomataApplication.java
@@ -0,0 +1,13 @@
+package sanrandomstuff.beautifulautomata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulautomataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulautomataApplication.class, args);
+	}
+
+}
---
+++ backend/beautifulautomata/src/test/java/sanrandomstuff/beautifulautomata/BeautifulautomataApplicationTests.java
@@ -0,0 +1,13 @@
+package sanrandomstuff.beautifulautomata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulautomataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
